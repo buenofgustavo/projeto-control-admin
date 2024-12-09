2237ecf78780c7c07ea388bde33a889b23d2c2bb
buenofgustavo <buenofgustavo@gmail.com>
2024-12-09 15:10:01 -0300

    Script que pega informações dos computadores adicionados

---
+++ sitema-informacoes-pc-main/VISUAL_RODOPAR.py
@@ -0,0 +1,34 @@
+from src.main.esconder_janela.esconder_janela import hide_console_window
+from src.main.conexao_a_internet.verificar_conxão import is_connected
+from src.main.obter_e_registrar.obter_e_registrar import obter_e_registrar_informacoes
+from src.main.token.gerar_token import gerar_token
+from src.main.fazer_solicitacao_http.solicitacao import fazer_solicitacao
+
+import time
+
+def main():
+
+    hide_console_window()
+
+    while True:
+        time.sleep(0 * 60)
+        if is_connected():
+            dados = obter_e_registrar_informacoes()
+            token = gerar_token(dados[2])
+            print(token)
+            response = fazer_solicitacao(token, dados)
+            print(response)
+            if response.status_code == 200:
+                print("Solicitação bem-sucedida!")
+            else:
+                print("Falha na solicitação. Status code:", response.status_code)
+
+        else:
+            print("Sem conexão com a internet. Aguardando...")
+        
+        time.sleep(30 * 60)
+
+        
+
+if __name__ == "__main__":
+    main()
---
+++ sitema-informacoes-pc-main/src/main/fazer_solicitacao_http/solicitacao.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def fazer_solicitacao(token, dados):
+    url = 'http://187.32.51.50:7892/computadores'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+        'Origin': 'http://your_origin_here.com'
+    }
+    dados_json = criar_json_nomeado(dados)
+    response = requests.post(url, json=dados_json, headers=headers)
+    return response
+
+def criar_json_nomeado(dados):
+    # Definindo os nomes dos campos
+    nomes_campos = [
+        "nomeUsuario",
+        "nomeComputador",
+        "enderecoMac",
+        "localizacao",
+        "memoriaRam",
+        "capacidadeArmazenamento",
+        "marca",
+        "modelo",
+        "processador",
+        "sistemaOperacional",
+        "makroInstalado",
+        "versaoMakro"
+        
+    ]
+
+    # Criando um dicionário com os nomes dos campos como chaves
+    json_nomeado = {nome: valor for nome, valor in zip(nomes_campos, dados)}
+
+    return json_nomeado
